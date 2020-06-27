a7a62065bd3cf47d9f0564dc314eb0b59e681581
darkki <baka.prototype@gmail.com>
2020-06-27 13:00:19 +0300

    added time_convert function

--- gl-isf.py
+++ gl-isf.py
@@ -17,3 +17,18 @@ class app_info:
     name = "GameList-import.scrape.format"
     version = "20200623|Dev"
     by = "darkk!"
+
+def time_convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    if int(hour) == 0 and int(minutes) == 0 and int(seconds) == 0:
+        return("~")
+    elif int(hour) == 0 and int(minutes) == 0:
+        return(f"{int(seconds)}s")
+    elif int(hour) == 0:
+        return(f"{int(minutes)}m {int(seconds)}s")
+    else:
+        return(f"{int(hour)}h {int(minutes)}m {int(seconds)}s")
